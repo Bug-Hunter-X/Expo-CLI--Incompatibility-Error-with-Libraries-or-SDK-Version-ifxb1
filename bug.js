7ad1f53a6fc8802d85f8cb3ae349ca3d99272162
BugHunter-X <askzenai@gmail.com>
2025-01-02 06:56:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+This error occurs when using the Expo CLI and attempting to run an app that uses a library or feature not compatible with the current Expo SDK version or environment.  For example, using a library that requires a newer version of React Native or a feature that's been deprecated.
+
+```javascript
+// Example code that might produce the error (using an outdated library)
+import OutdatedLibrary from 'outdated-library';
+```
